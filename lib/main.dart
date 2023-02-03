bd414751ca49cb5c1dda027b0e559f96a6b94c4f
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2023-02-03 19:18:57 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:taskify_project/Screens/Login/signup.dart';
 import 'Screens/splash.dart';
 
 void main() {
