6a12e2545bbe62776caa6e279338ea56c2a68efd
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2023-02-16 22:24:25 +0530

    bugs fixed

--- lib/Screens/Login/login.dart
+++ lib/Screens/Login/login.dart
@@ -62,154 +62,163 @@ class _LoginState extends State<Login> {
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Column(children: [
-        SizedBox(height: 95),
-        Container(
-          margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text(
-              _texts[_index],
-              style: TextStyle(
-                  fontSize: 70,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFFF5722)),
-            ),
-            Text(
-              'Sign into your Account',
-              style: TextStyle(
-                  fontSize: 20, color: Color.fromARGB(154, 255, 109, 64)),
-            ),
-            SizedBox(height: 50),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                        spreadRadius: 7,
-                        blurRadius: 10,
-                        offset: Offset(1, 1),
-                        color: Colors.deepOrangeAccent.withOpacity(0.1))
-                  ]),
-              child: TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (email) =>
-                    email != null && !EmailValidator.validate(email)
-                        ? '          Enter a valid email'
-                        : null,
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    hintText: 'Enter Email',
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.deepOrange,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide:
-                            BorderSide(color: Colors.white, width: 1.0)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide:
-                            BorderSide(color: Colors.white, width: 1.0))),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                        spreadRadius: 7,
-                        blurRadius: 10,
-                        offset: Offset(1, 1),
-                        color: Colors.deepOrangeAccent.withOpacity(0.1))
-                  ]),
-              child: TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) => value != null && value.length < 6
-                    ? '          Enter min 6 characters'
-                    : null,
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                    hintText: 'Enter Password',
-                    prefixIcon: Icon(
-                      Icons.password,
-                      color: Colors.deepOrange,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide:
-                            BorderSide(color: Colors.white, width: 1.0)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide:
-                            BorderSide(color: Colors.white, width: 1.0))),
-              ),
-            ),
-            SizedBox(height: 25),
-            Row(
-              children: [
-                GestureDetector(
-                  child: Text(
-                    'Forgot your Password?',
-                    style: TextStyle(fontSize: 15, color: Colors.red),
-                  ),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => ForgotPass()));
-                  },
-                ),
-              ],
-            ),
-          ]),
-        ),
-        SizedBox(height: 40),
-        Container(
-          width: 230,
-          height: 60,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            gradient: const LinearGradient(
-              colors: [Colors.redAccent, Colors.deepOrangeAccent],
-            ),
-          ),
-          child: MaterialButton(
-            minWidth: 230,
-            height: 60,
-            onPressed: signin,
-            child: Text(
-              "Login",
-              style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25),
-            ),
-          ),
-        ),
-        SizedBox(height: w * 0.2),
-        RichText(
-            text: TextSpan(
-                text: "Don't have an account?",
-                style: TextStyle(
-                    fontSize: 17, color: Color.fromARGB(255, 255, 109, 64)),
+      body: Container(
+      
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        
+          // SizedBox(height: 65),
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+            child:
+                Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start, 
                 children: [
-              TextSpan(
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () => Get.to(() => SignUp()),
-                text: " Create Account",
+              Text(
+                _texts[_index],
                 style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold),
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFF5722)),
               ),
-            ])),
-      ]),
+              Text(
+                'Sign into your Account',
+                style: TextStyle(
+                    fontSize: 20, color: Color.fromARGB(154, 255, 109, 64)),
+              ),
+              SizedBox(height: 50),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          spreadRadius: 7,
+                          blurRadius: 10,
+                          offset: Offset(1, 1),
+                          color: Colors.deepOrangeAccent.withOpacity(0.1))
+                    ]),
+                child: TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (email) =>
+                      email != null && !EmailValidator.validate(email)
+                          ? '          Enter a valid email'
+                          : null,
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      hintText: 'Enter Email',
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.deepOrange,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0))),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          spreadRadius: 7,
+                          blurRadius: 10,
+                          offset: Offset(1, 1),
+                          color: Colors.deepOrangeAccent.withOpacity(0.1))
+                    ]),
+                child: TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) => value != null && value.length < 6
+                      ? '          Enter min 6 characters'
+                      : null,
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'Enter Password',
+                      prefixIcon: Icon(
+                        Icons.password,
+                        color: Colors.deepOrange,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0))),
+                ),
+              ),
+              SizedBox(height: 25),
+              Row(
+                children: [
+                  GestureDetector(
+                    child: Text(
+                      'Forgot your Password?',
+                      style: TextStyle(fontSize: 15, color: Colors.red),
+                    ),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => ForgotPass()));
+                    },
+                  ),
+                ],
+              ),
+            ]),
+          ),
+          SizedBox(height: 40),
+          Container(
+            width: 230,
+            height: 60,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              gradient: const LinearGradient(
+                colors: [Colors.redAccent, Colors.deepOrangeAccent],
+              ),
+            ),
+            child: MaterialButton(
+              minWidth: 230,
+              height: 60,
+              onPressed: signin,
+              child: Text(
+                "Login",
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25),
+              ),
+            ),
+          ),
+          SizedBox(height: w * 0.13),
+          RichText(
+              text: TextSpan(
+                  text: "Don't have an account?",
+                  style: TextStyle(
+                      fontSize: 17, color: Color.fromARGB(255, 255, 109, 64)),
+                  children: [
+                TextSpan(
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => Get.to(() => SignUp()),
+                  text: " Create Account",
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold),
+                ),
+              ])),
+        ]),
+      ),
     );
   }
 
--- lib/Screens/Login/signup.dart
+++ lib/Screens/Login/signup.dart
@@ -48,168 +48,172 @@ class _SignUpState extends State<SignUp> {
       resizeToAvoidBottomInset: false,
       body: Form(
         key: formKey,
-        child: Column(children: [
-          SizedBox(height: 50),
-          Container(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: h * 0.18,
-                ),
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: AssetImage("assets/avatar.jpg"),
-                )
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(height: 45),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                          spreadRadius: 7,
-                          blurRadius: 10,
-                          offset: Offset(1, 1),
-                          color: Colors.deepOrangeAccent.withOpacity(0.1))
-                    ]),
-                child: TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) => value != null && value.length < 3
-                      ? '          Enter a valid Name'
-                      : null,
-                  controller: nameController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      hintText: 'Enter Name',
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.deepOrange,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 1.0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 1.0))),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                          spreadRadius: 7,
-                          blurRadius: 10,
-                          offset: Offset(1, 1),
-                          color: Colors.deepOrangeAccent.withOpacity(0.1))
-                    ]),
-                child: TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (email) =>
-                      email != null && !EmailValidator.validate(email)
-                          ? '          Enter a valid email'
-                          : null,
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      hintText: 'Enter Email',
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.deepOrange,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 1.0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 1.0))),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                          spreadRadius: 7,
-                          blurRadius: 10,
-                          offset: Offset(1, 1),
-                          color: Colors.deepOrangeAccent.withOpacity(0.1))
-                    ]),
-                child: TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) => value != null && value.length < 6
-                      ? '          Enter min 6 characters'
-                      : null,
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: 'Enter Password',
-                      prefixIcon: Icon(
-                        Icons.password,
-                        color: Colors.deepOrange,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 1.0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 1.0))),
-                ),
-              ),
-            ]),
-          ),
-          SizedBox(height: 55),
-          Container(
-            width: 230,
-            height: 50,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              gradient: const LinearGradient(
-                colors: [Colors.redAccent, Colors.deepOrangeAccent],
+        child: Container(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // SizedBox(height: 50),
+            Container(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: h * 0.18,
+                  ),
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage("assets/avatar.jpg"),
+                  )
+                ],
               ),
             ),
-            child: MaterialButton(
-              minWidth: 230,
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child:
+                  Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                SizedBox(height: 45),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            spreadRadius: 7,
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
+                            color: Colors.deepOrangeAccent.withOpacity(0.1))
+                      ]),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) => value != null && value.length < 3
+                        ? '          Enter a valid Name'
+                        : null,
+                    controller: nameController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Name',
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.deepOrange,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0))),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            spreadRadius: 7,
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
+                            color: Colors.deepOrangeAccent.withOpacity(0.1))
+                      ]),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (email) =>
+                        email != null && !EmailValidator.validate(email)
+                            ? '          Enter a valid email'
+                            : null,
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Email',
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.deepOrange,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0))),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            spreadRadius: 7,
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
+                            color: Colors.deepOrangeAccent.withOpacity(0.1))
+                      ]),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) => value != null && value.length < 6
+                        ? '          Enter min 6 characters'
+                        : null,
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Password',
+                        prefixIcon: Icon(
+                          Icons.password,
+                          color: Colors.deepOrange,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0))),
+                  ),
+                ),
+              ]),
+            ),
+            SizedBox(height: 55),
+            Container(
+              width: 230,
               height: 50,
-              onPressed: () {
-                sign();
-              },
-              child: Text(
-                "SignUp",
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                gradient: const LinearGradient(
+                  colors: [Colors.redAccent, Colors.deepOrangeAccent],
+                ),
+              ),
+              child: MaterialButton(
+                minWidth: 230,
+                height: 50,
+                onPressed: () {
+                  sign();
+                },
+                child: Text(
+                  "SignUp",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          RichText(
-              text: TextSpan(
-                  recognizer: TapGestureRecognizer()..onTap = () => Get.back(),
-                  text: "Have an account?",
-                  style: TextStyle(fontSize: 17, color: Colors.deepOrange))),
-        ]),
+            SizedBox(
+              height: 25,
+            ),
+            RichText(
+                text: TextSpan(
+                    recognizer: TapGestureRecognizer()..onTap = () => Get.back(),
+                    text: "Have an account?",
+                    style: TextStyle(fontSize: 17, color: Colors.deepOrange))),
+          ]),
+        ),
       ),
     );
   }
